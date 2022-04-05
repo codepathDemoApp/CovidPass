578110c622e39f9442fc9106ca91effe521ecb98
ZLYONZ <lyonzlt@gmail.com>
2022-04-05 19:37:22 -0400

    updated

---
+++ CovidPass/CovidPass/HistoryCell.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryCell.swift
+//  CovidPass
+//
+//  Created by LYON on 4/5/22.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
