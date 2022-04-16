760233731ec56e2bf1029b05ba6e4dec5786ba6c
Hailey-Tran <h8tran@ucsd.edu>
2022-04-15 21:35:56 -0700

    profile first

---
+++ CovidPass/CovidPass/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewController.swift
+//  CovidPass
+//
+//  Created by Hailey Tran on 4/15/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var logoutButton: UIBarButtonItem!
+    
+    @IBOutlet weak var qrCodeButton: UIBarButtonItem!
+    
+    @IBOutlet weak var editProfileButton: UIButton!
+    
+    @IBAction func saveProfileButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CovidPass/CovidPass/editProfileController.swift
@@ -0,0 +1,45 @@
+//
+//  editProfileController.swift
+//  CovidPass
+//
+//  Created by Hailey Tran on 4/15/22.
+//
+
+import UIKit
+
+class editProfileController: UIViewController {
+
+    @IBOutlet weak var editNameTextField: UITextField!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    
+    @IBOutlet weak var genderTextField: UITextField!
+    
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
