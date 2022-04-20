7f2f43633b4607938d33905824e9fdf2e5d02630
Hailey-Tran <h8tran@ucsd.edu>
2022-04-19 21:25:06 -0700

    profile progress 1.2

--- CovidPass/CovidPass/UserProfileVC.swift
+++ CovidPass/CovidPass/UserProfileVC.swift
@@ -8,18 +8,75 @@
 import UIKit
 import Parse
 
-class UserProfileVC: UIViewController {
+class UserProfileVC: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var editProfileButton: UIButton!
+    @IBOutlet weak var editNameField: UITextField!
+    
+    @IBOutlet weak var editUsernameField: UITextField!
+    
+    @IBOutlet weak var editPasswordField: UITextField!
+    
+    @IBOutlet weak var editEmailField: UITextField!
+    
+    @IBOutlet weak var editGenderField: UITextField!
+    
+    @IBOutlet weak var editPhoneNumberField: UITextField!
+    
+    @IBOutlet weak var editAddressField: UITextField!
+    
+    @IBOutlet weak var saveProfileButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        editNameField.delegate = self
+        editUsernameField.delegate = self
+        editPasswordField.delegate = self
+        editEmailField.delegate = self
+        editGenderField.delegate = self
+        editPhoneNumberField.delegate = self
+        editAddressField.delegate = self
+        
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func saveProfile(_ sender: Any) {
+        let name = editNameField.text!
+        let username = editUsernameField.text!
+        let password = editPasswordField.text!
+        let email = editEmailField.text!
+        let gender = editGenderField.text!
+        let phone = editPhoneNumberField.text!
+        let address = editAddressField.text!
         
-    @IBAction func saveProfileButton(_ sender: Any) {
+        let profile = PFObject(className: "User")
+        profile["name"] = editNameField.text
+        profile["username"] = editUsernameField.text
+        
+        profile.saveInBackground() { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved")
+            } else {
+                print("error")
+            }
+        }
+        
     }
+    
+    
+     
+    func saveInfo(_ textField: UITextField) {
+        if (!(editNameField.text?.count == 0) || !(editUsernameField.text?.count == 0) || !(editPasswordField.text?.count == 0) || !(editEmailField.text?.count == 0) || !(editGenderField.text?.count == 0) || !(editPhoneNumberField.text?.count == 0) || !(editAddressField.text?.count == 0)) {
+            saveProfileButton.isUserInteractionEnabled = true
+            saveProfileButton.tintColor = UIColor.init(red: 36/255, green: 122/255, blue: 255/255, alpha: 1)
+        } else {
+            saveProfileButton.isUserInteractionEnabled = false
+            saveProfileButton.tintColor = UIColor.init(red: 36/255, green: 122/255, blue: 255/255, alpha: 0.5)
+        }
+    }
+
     
     
     @IBAction func onLogOut(_ sender: Any) {
